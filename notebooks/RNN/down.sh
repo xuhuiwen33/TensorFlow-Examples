60c95d3740ceb03ecaa57b3c5e45339176076b12
cesc-park <cs.park@vision.snu.ac.kr>
2016-10-28 15:09:05 +0900

    update down scripts

---
+++ notebooks/RNN/down.sh
@@ -0,0 +1,8 @@
+cd /tmp/data
+rm -rf ./*
+wget --content-disposition goo.gl/GnNuGK
+wget --content-disposition goo.gl/6uHySY
+wget --content-disposition goo.gl/jUHeOG
+wget --content-disposition goo.gl/8F2ALB
+cd ~/
+jupyter notebook
